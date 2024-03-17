94192615e2573c032e2e7d255d35b4f24cf3f772
Nikolay Nikiforov <lnikolayl97l@gmail.com>
2024-03-17 11:56:10 +0300

    Jest, Prettier, ESlint configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import { resolve } from 'path';
-import { defineConfig } from 'vite';
-import dts from 'vite-plugin-dts';
- 
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'json-function-pipeline',
-      fileName: 'index',
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "json-function-pipeline",
+      fileName: "index",
     },
   },
   plugins: [dts()],
